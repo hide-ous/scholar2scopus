20524eb327fe590f4b0d89b6071eb40a2eebd07c
hide-ous <mattia.samory@gmail.com>
2022-01-21 17:11:35 +0100

    * modularized scholar code

---
+++ scholar.py
@@ -0,0 +1,40 @@
+import json
+
+from scholarly import scholarly
+
+
+def get_author(name):
+    search_query = scholarly.search_author(name)
+    first_author_result = next(search_query)
+    author = scholarly.fill(first_author_result)
+    return author
+
+
+def get_publications(author):
+    return list(map(scholarly.fill, author['publications']))
+
+
+def get_citations(publication):
+    return list(map(scholarly.fill, scholarly.citedby(publication)))
+
+
+def scrape_author_publications_citations(name):
+    author = get_author(name)
+
+    with open('author.json', 'w+', encoding='utf8') as f:
+        json.dump({k: v for k, v in author.items() if k != 'source'}, f)
+
+    publications = dict(enumerate(get_publications(author)))
+    with open('publications.json', 'w+', encoding='utf8') as f:
+        json.dump({k: v for k, v in publications.items() if k != 'source'}, f)
+
+    citations = dict()
+    for idx, publication in publications.items():
+        current_citations = get_citations(publication)
+        citations[idx] = current_citations
+    with open('citations.json', 'w+', encoding='utf8') as f:
+        json.dump({k: v for k, v in citations.items() if k != 'source'}, f)
+
+
+if __name__ == '__main__':
+    scrape_author_publications_citations('Your Name')
